0495f45a803a085a93b3c247fb70dcf77545d31a
Thomas Qvarnström <tqvarnst@redhat.com>
2021-02-24 12:49:53 +0100

    added more default data

--- price-service/src/main/resources/import.sql
+++ price-service/src/main/resources/import.sql
@@ -1,5 +1,12 @@
 INSERT INTO Price(id,salesId,country,currency,price) VALUES (nextval('hibernate_sequence'),'DAIRY_00034','SE','SEK',11.90);
+INSERT INTO Price(id,salesId,country,currency,price) VALUES (nextval('hibernate_sequence'),'DAIRY_00036','SE','SEK',12.90);
+INSERT INTO Price(id,salesId,country,currency,price) VALUES (nextval('hibernate_sequence'),'DAIRY_00037','SE','SEK',43.90);
+INSERT INTO Price(id,salesId,country,currency,price) VALUES (nextval('hibernate_sequence'),'DAIRY_00034','SE','SEK',55.90);
 INSERT INTO Price(id,salesId,country,currency,price) VALUES (nextval('hibernate_sequence'),'CANDY_01345','SE','SEK',6.95);
+INSERT INTO Price(id,salesId,country,currency,price) VALUES (nextval('hibernate_sequence'),'CANDY_01346','SE','SEK',5.95);
+INSERT INTO Price(id,salesId,country,currency,price) VALUES (nextval('hibernate_sequence'),'CANDY_01347','SE','SEK',8.95);
+INSERT INTO Price(id,salesId,country,currency,price) VALUES (nextval('hibernate_sequence'),'CANDY_01348','SE','SEK',12.95);
 INSERT INTO Price(id,salesId,country,currency,price) VALUES (nextval('hibernate_sequence'),'FRUIT_00003','SE','SEK',22.99);
-
-
+INSERT INTO Price(id,salesId,country,currency,price) VALUES (nextval('hibernate_sequence'),'FRUIT_00004','SE','SEK',25.99);
+INSERT INTO Price(id,salesId,country,currency,price) VALUES (nextval('hibernate_sequence'),'FRUIT_00005','SE','SEK',12.99);
+INSERT INTO Price(id,salesId,country,currency,price) VALUES (nextval('hibernate_sequence'),'FRUIT_00006','SE','SEK',36.99);
